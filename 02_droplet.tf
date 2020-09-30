502f5f9111600ecd3c00a45b9d42bed69d5f74b6
David Diaz <diazdavid.info@gmail.com>
2020-09-30 18:18:53 +0200

    droplet con debian

--- 02_droplet.tf
+++ 02_droplet.tf
@@ -2,8 +2,8 @@
 # Creamos el droplet
 
 resource "digitalocean_droplet" "testing" {
-  image     = "ubuntu-18-04-x64"
-  name      = "testing-1"
+  image     = "debian-10-x64"
+  name      = "debian-1"
   region    = "lon1"
   size      = "s-1vcpu-1gb"
   user_data = "${file("userdata.yaml")}"
