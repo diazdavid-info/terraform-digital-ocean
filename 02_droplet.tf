e54e3ea9472a6cb198c5df7aa4d74500ce4fa84b
David Diaz <diazdavid.info@gmail.com>
2020-10-01 06:27:27 +0200

    droplet con wireguard

--- 02_droplet.tf
+++ 02_droplet.tf
@@ -2,10 +2,25 @@
 # Creamos el droplet
 
 resource "digitalocean_droplet" "testing" {
-  image     = "debian-10-x64"
-  name      = "debian-1"
-  region    = "lon1"
-  size      = "s-1vcpu-1gb"
-  user_data = "${file("userdata.yaml")}"
-  ssh_keys  = ["${digitalocean_ssh_key.ssh-key.fingerprint}"]
+  image = "debian-10-x64"
+  name = "debian-1"
+  region = "fra1"
+  size = "s-1vcpu-1gb"
+  user_data = file("userdata.yaml")
+  ssh_keys = [
+    digitalocean_ssh_key.ssh-key.fingerprint
+  ]
+
+  connection {
+    type = "ssh"
+    user = "root"
+    agent = true
+    host = self.ipv4_address
+    private_key = file("~/.ssh/terraform")
+  }
+
+  provisioner "file" {
+    source = "docker-compose.yml"
+    destination = "/root/docker-compose.yml"
+  }
 }
--- _provider.tf
+++ _provider.tf
@@ -1,7 +1,16 @@
 variable "digitalocean_token" {}
 
+terraform {
+  required_providers {
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+      version = "~> 1.22.1"
+    }
+  }
+}
+
 # Configure the DigitalOcean Provider
 provider "digitalocean" {
-  token = "${var.digitalocean_token}"
+  token = var.digitalocean_token
 }
 
